bb75b18d887cd093867d5f087a740711cb57d038
jkaste03 <johanneskaste@gmail.com>
2025-03-17 20:47:32 +0100

    Add performance logging for league rounds execution time

--- src/main/java/com/github/jkaste03/uefa_cc_sim/UefaCCSim.java
+++ src/main/java/com/github/jkaste03/uefa_cc_sim/UefaCCSim.java
@@ -4,8 +4,8 @@ import com.github.jkaste03.uefa_cc_sim.model.Rounds;
 
 public class UefaCCSim {
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
         Rounds rounds = new Rounds();
+        long startTime = System.currentTimeMillis();
         rounds.run();
         long endTime = System.currentTimeMillis();
         System.out.println("Seeding and drawing league rounds took " + (endTime - startTime) + " milliseconds.");
